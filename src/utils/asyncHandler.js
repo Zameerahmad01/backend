754dd0301c87857f512e862acdd0fc8d5e5771b5
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-09-15 22:13:53 +0500

    express cookie parser cores and utils configuration

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next);
+//     } catch (error) {
+//         res.status(500).json({
+//             success: false,
+//             message: error.message,
+//         });
+//     }
+// }
+
+export { asyncHandler };
