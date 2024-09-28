f4f0e7b401da558bb1c1dc34741c9425c7e0a22a
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-09-28 18:04:38 +0500

    add comments for the routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -39,13 +39,15 @@ router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
+//update user details patch because we are updating only some fields not all fields
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
-
+//update avatar and cover image using patch because we are updating only one field upload.single because we are uploading only one file
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
 router
   .route("/cover")
   .patch(verifyJWT, upload.single("cover"), updateCoverImage);
 
+//get user channel profile and watch history get because we are not updating anything colon is used to get the username from the url
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
