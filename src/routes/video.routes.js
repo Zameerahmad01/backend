603ecc5a1baabbefe78c2dd2f8e7be2a44f665e8
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-01 14:53:30 +0500

    add routes for the video controllers

---
+++ src/routes/video.routes.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  uploadVideo,
+  updateVideo,
+  deleteVideo,
+  getVideoById,
+} from "../controllers/video.controller.js";
+
+const router = Router();
+router.use(verifyJWT);
+
+router.route("/uploadVideo").post(
+  upload.fields([
+    {
+      name: "videoFile",
+      maxCount: 1,
+    },
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+  ]),
+  uploadVideo
+);
+
+router
+  .route("/:videoId")
+  .get(getVideoById)
+  .delete(deleteVideo)
+  .patch(upload.single("thumbnail"), updateVideo);
+
+export default router;
